ed7c67f14d4b7e5b621ae846fcf160b133b563c8
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-11-22 20:01:51 +0100

    updating xml files

--- user-management-api/src/test/java/com/griddynamics/gridu/qa/user/tests/e2e/UserManagementE2EBaseTest.java
+++ user-management-api/src/test/java/com/griddynamics/gridu/qa/user/tests/e2e/UserManagementE2EBaseTest.java
@@ -1,14 +1,14 @@
 package com.griddynamics.gridu.qa.user.tests.e2e;
 
 import com.griddynamics.gridu.qa.user.client.UserManagementClient;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeMethod;
 
 public class UserManagementE2EBaseTest {
 
     public static final int USER_MANAGEMENT_PORT = 8080;
     protected UserManagementClient userManagementClient;
 
-    @BeforeSuite
+    @BeforeMethod
     public void setUp() {
         userManagementClient = new UserManagementClient();
     }
